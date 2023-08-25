4f46b1708e91be37a1b56462dbe8a3386064094a
ths22115 <xselymx@gmail.com>
2023-08-25 12:18:56 -0400

    updated timer

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function changeActive(target_page, current_page){
 
 
 // set the date we're counting down to
-var countDownDate = new Date("Aug 25, 2023 11:00:00").getTime();
+var countDownDate = new Date("Aug 25, 2023 18:15:00").getTime();
 
 // Update the count down every 1 second
 var timer = setInterval(function() {
